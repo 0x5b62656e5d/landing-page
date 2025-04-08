c9a6f6721e7635f75be328414a6f2bae22e7d900
0x5b62656e5d <dev.benjamink@gmail.com>
2025-04-08 09:19:26 +0800

    Improve page performance

--- src/ts/typewriter.ts
+++ src/ts/typewriter.ts
@@ -1,43 +1,26 @@
 class Typewriter {
     private typewriterElement: Element;
     private displayedText: string = "";
-    private period: number;
     private fullText: string;
-    private isDeleting: boolean = false;
 
-    constructor(typewriter: Element, text: string, period: string) {
+    constructor(typewriter: Element, text: string) {
         this.typewriterElement = typewriter;
         this.fullText = text;
-        this.period = parseInt(period, 10);
         this.write();
     }
 
     private write() {
-        if (this.isDeleting) {
-            this.displayedText = this.fullText.substring(0, this.displayedText.length - 1);
-        } else {
-            this.displayedText = this.fullText.substring(0, this.displayedText.length + 1);
-        }
+        this.displayedText = this.fullText.substring(0, this.displayedText.length + 1);
 
         this.typewriterElement.textContent = this.displayedText;
 
         let delta = 150 - Math.random() * 100;
 
-        if (this.isDeleting) {
-            delta /= 2;
+        if (this.displayedText !== this.fullText) {
+            setTimeout(() => {
+                this.write();
+            }, delta + (this.displayedText.length === 1 ? 400 : 0));
         }
-
-        if (!this.isDeleting && this.displayedText === this.fullText) {
-            delta = this.period;
-            this.isDeleting = true;
-        } else if (this.isDeleting && this.displayedText === "") {
-            this.isDeleting = false;
-            delta = 500;
-        }
-
-        setTimeout(() => {
-            this.write();
-        }, delta);
     }
 }
 
@@ -46,10 +29,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (const typewriter of typewriters) {
         const text = typewriter.getAttribute("data-text");
-        const period = typewriter.getAttribute("data-duration");
 
-        if (text && period) {
-            new Typewriter(typewriter, text, period);
+        if (text) {
+            new Typewriter(typewriter, text);
         }
     }
 });
