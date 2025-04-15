477f07a27f2e8a718de60adf36111108f9c2d394
0x5b62656e5d <dev.benjamink@gmail.com>
2025-04-15 11:47:00 -0500

    Refactor parallax.ts

--- src/ts/parallax.ts
+++ src/ts/parallax.ts
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (/AppleWebKit/.test(navigator.userAgent) && /Apple/.test(navigator.vendor)) {
         const section = document.querySelectorAll("section") as NodeListOf<HTMLElement>;
 
-        section.forEach(e => e.style.setProperty("transform", "translateZ(0)"));
-        section.forEach(e => e.style.setProperty("will-change", "background-position"));
-        section.forEach(e => e.style.setProperty("backface-visibility", "hidden"));
-        section.forEach(e => e.style.setProperty("-webkit-transform", "translateZ(0)"));
-        section.forEach(e => e.style.setProperty("-webkit-backface-visibility", "hidden"));
+        section.forEach(e => {
+            e.style.setProperty("transform", "translateZ(0)");
+            e.style.setProperty("will-change", "background-position");
+            e.style.setProperty("backface-visibility", "hidden");
+            e.style.setProperty("-webkit-transform", "translateZ(0)");
+            e.style.setProperty("-webkit-backface-visibility", "hidden");
+        });
     }
 });
